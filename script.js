4c4f7f0fe12ad93025f2db9fdd778a67aef65683
birnaleriya <riya.birnale31@gmail.com>
2024-04-20 13:24:24 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,196 @@
+<!-- <a href="https://www.google.com">Click Me</a> -->
+
+<header>
+    <table>
+        <tr>
+            <td>
+                <a href="https://www.github.com/birnaleriya">
+                    <img style="float: left;" width="300" src="myphoto.jpg"
+                        alt="Riya Birnale's photo">
+                </a>
+            </td>
+            <td>
+                <h1>Riya Birnale</h1>
+                <p><i>Student at<a target="_blank" href="kjsit.somaiya.edu">KJ Somaiya Institute of Technology</a></i></p>
+                <p>I am currently working on Web Development and am willing to undertake work related to web development.</p>
+                
+                <nav>
+                    <ul>
+                        <li><a href="#education">Education</a></li>
+                        <li><a href="#internships">Internships</a></li>
+                        <li><a href="#projects">Projects</a></li>
+                        <li><a href="#co-curricular-activities">Co-curricular-activities</a></li>
+                        <li><a href="#contactme">Connect with Me</a></li>
+                    </ul>
+                </nav>
+
+            </td>
+        </tr>
+    </table>
+
+
+    <hr>
+</header>
+
+<!-- Header ends -->
+
+<main>
+
+    <section id="education">
+        <h2>Education</h2>
+        <table border="">
+            <tr>
+                <th>Course</th>
+                <th>Institute</th>
+                <th>Year</th>
+            </tr>
+            <tr>
+                <td>BTech in AI&DS</td>
+                <td>KJSIT, Mumbai</td>
+                <td>2nd year</td>
+            </tr>
+            <tr>
+                <td>12<sup>th</sup> Grade</td>
+                <td>Ligade Patil Junior College of Science, Karad</td>
+                <td>2022</td>
+            </tr>
+            <tr>
+                <td>10<sup>th</sup> Grade</td>
+                <td>Holy Family Convent High School, Karad</td>
+                <td>2020</td>
+            </tr>
+        </table>
+    </section>
+
+    <section id="internships">
+        <h2>Internships</h2>
+        <article>
+            <p><strong>Flutter(Dart)</strong>FrontEnd & BackEnd</p>
+            <ul>
+                <li>I learned Flutter FrontEnd and BackEnd in a workshop internship which was held in KJSIT conducted by Sushant Waghulde.</li>
+                <li>I have developed applications using the knowledge gained from this internship.</li>
+            </ul>
+            <p><strong>Web/Mobile App Designing and Machine Learning for Project Development</strong></p>
+            <ul>
+                <li>This Training+Hands-On project internship was organized by SDC-REDEX by KJSIT.</li>
+                <li>I developed an E-Commerce website using HTML, CSS and Javascript in this internship program.</li>
+            </ul>
+        </article>
+    </section>
+
+    <section id="projects">
+        <h2>Projects</h2>
+            <li>
+                <article>
+                    <a href="https://github.com/e-commerce-website">
+                        <h3>E-Commerce Website</h3>
+                    </a>
+                    <ul>
+                        <li>
+                            <p>I have created an E-Commerce Website using HTML, CSS and Javascript.</p>
+                        </li>
+                        <li>
+                            <p>This website provides the information of the products we wish to buy.</p>
+                        </li>
+                    </ul>
+                    <a href="https://github.com/birnaleriya/Cat-Clock">
+                        <h3>Cat-Clock</h3>
+                    </a>
+                    <ul>
+                        <li>
+                            <p>I have created a website using HTML,CSS and Javascript</p>
+                        </li>
+                        <li>
+                            <p>This website is designed to give cat faces as per the entered schedules.</p>
+                        </li>
+                    </ul>
+                    <h2>Here are some of my group projects<h2>
+                        <h3>Rajnikanth</h3>
+                    <ul>
+                        <li>
+                            <p>This application is created using Flutter (Dart) and whimsical for designing purpose.</p>
+                        </li>
+                        <li>
+                            <p>This app is designed to give latest updates for the movies enacted or directed by the Rajnikanth Family.</p>
+                        </li>
+                    </ul>
+                     <h3>Filmy</h3>
+                    <ul>
+                        <li>
+                            <p>This application is created using Flutter in the Flutter internship training program.</p>
+                        </li>
+                        <li>
+                            <p>This app is designed to give the details of the latest films released including the storyline, ratings, popularity etc..</p>
+                        </li>
+                    </ul>
+                        <h3>Notes App</h3>
+                    <ul>
+                        <li>
+                            <p>This application is created using using Flutter firebase.</p>
+                        </li>
+                        <li>
+                            <p>This application is designed to write and save the notes of any sort.The data is stored on Flutterfire.</p>
+                        </li>
+                    </ul>
+                        <h3>Java Quiz Game</h3>
+                    <ul>
+                        <li>
+                            <p>This website is designed using Java Swing Components.</p>
+                        </li>
+                        <li>
+                            <p>This website is purely designed to play a Quiz game which within a certain bounded time limit.One can also retake the Quiz after finishing the previous attempt.</p>
+                        </li>
+                    </ul>
+                        <h3>Product Price Tracker</h3>
+                    <ul>
+                        <li>
+                            <p>This website is designed using python web scraping tool named Beautiful soup and other Python libraries for building the website.</p>
+                        </li>
+                        <li>
+                            <p>This website is purely designed for tracking the price of the products we wish to buy from Flipkart at their lowest prices.An email would be sent to the user when the price of the product decreases to upto the target set by the customer.</p>
+                        </li>
+                    </ul>
+                </article>
+            </li>
+        <hr>
+                    </section>
+    <section id="co-curricular-activities">
+        <h2>Co-curricular Activites</h2>
+            <article>
+                <h3>NSS Volunteer</h3>
+                <ul>
+                    <li>
+                        <p>I am a NSS Volunteer as well as the Joint Digital Media Manager(DMMM) of the NSS unit of KJSIT.</p>
+                    </li>
+                    <li>
+                        <p>I have volunteered various social events such as Blood Donation, Clean-up Drives, Tree Plantation Drives, Stray-Dog Feeding and Teaching Children Drives.</p>
+                    </li>
+                </ul>
+            </article>
+        </li>
+</main>
+
+<aside>
+
+</aside>
+
+<!-- Main Section Ends -->
+
+<footer id="contactme">
+    <h2 id="contact">Connect With Me</h2>
+    <p>Email: <a href="riya.birnale31@gmail.com">riya.birnale31@gmail.com</a></p>
+    <p>Github: github.com/birnaleriya</p>
+    <p>LinkedIn: <a href="https://www.linkedin.com/in/riya-birnale-79335125b/">LinkedIn_Riya</a></p>
+
+    <a href="aboutus.html">
+        <p>About Us</p>
+    </a>
+
+
+    <a href="#">Back To Top</a>
+
+</footer>
+
+
+
+<br><br><br>
